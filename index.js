9f75fa544b324f13b7c3215544ef9443edb643f3
Genki Kun <84325494+genkikun01@users.noreply.github.com>
2021-06-18 21:18:25 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
     console.log(Math.random())
+//And
+let len = 8;
+let str = "abcdefghijklmnopqrstuvwxyz";
+let strLen = str.length;
+let result = "";
+ 
+for (let i = 0; i < len; i++) {
+  result += str[Math.floor(Math.random() * strLen)];
+}
+ 
+console.log(result);
